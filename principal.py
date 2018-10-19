eca6ead5252442ce58ccee22259088ce2157f372
digoguima <43479901+digoguima@users.noreply.github.com>
2018-10-19 15:36:08 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,61 +1,82 @@
-import tkinter as tk
+
+
+
+
+
+
+
+
+
+
+	global mesa
+	global mesa
+	global mesa
+	global mesa
+	global mesa
+	global mesa
+	global mesa
+	global mesa 
+	global mesa 
+	global mesa 
+	global mesa 
+	global mesa_selecionada
+	janela_de_pedido = tk.Toplevel() 7u6y53t43r32t43vy4bu5j9k
+	mesa = 10
+	mesa = 11
+	mesa = 12
+	mesa = 2
+	mesa = 3
+	mesa = 4	
+	mesa = 5
+	mesa = 7
+	mesa = 7
+	mesa = 9
+	mesa = 9
+	print("Mesa 10")
+	print("Mesa 11")
+	print("Mesa 12"),
+	print("Mesa 2")
+	print("Mesa 3")
+	print("Mesa 4")
+	print("Mesa 5 ")
+	print("Mesa 6 ")
+	print("Mesa 7")
+	print("Mesa 8")
+	print("Mesa 9")
   
-
-consumos = [0]*12
-mesa= 0 
-
-#tk.Label(root, text="Bar Do Ze ").pack()
-#tk.Button(root, text="Olha e sou um botao")
-#tk.Entry(root, text ="qualquer coisa ")
-
-def banana1():
-   print("Mesa 1")
    global mesa
    mesa = 1
-
-def banana2():
-	print("Mesa 2")
-
-def banana3():
-	print("Mesa 3")
-
-def banana4():
-	print("Mesa 4")
-
-def banana5():
-		print("Mesa 5 ")
-def banana6():
-	print("Mesa 6 ")
-
-def banana7():
-	print("Mesa 7")
-
-def banana8():
-	print("Mesa 8")
-def banana9():
-	print("Mesa 9")
-def banana10():
-	print("Mesa 10")
+   print("Mesa 1")
+#tk.Button(root, text="Olha e sou um botao")
+#tk.Entry(root, text ="qualquer coisa ")
+#tk.Label(root, text="Bar Do Ze ").pack()
+consumos = [0]*12
 def	banana11():
-	print("Mesa 11")
+def banana1():
+def banana10():
 def banana12():
-	print("Mesa 12")		
-
+def banana2():
+def banana3():         
+def banana4():
+def banana5():
+def banana6():
+def banana7():
+def banana8():
+def banana9():
+def janela_de_pedido():
+import tkinter as tk
+mesa_selecionada= 
 root = tk.Tk()
-
-tk.Button(root, text="M1", command=banana1).grid(row=0, column=1)
-tk.Button(root, text="M2", command=banana2).grid(row=0, column=2)
-tk.Button(root, text="M3", command=banana3).grid(row=0, column=3)
-tk.Button(root, text="M4",command=banana4).grid(row=0, column=4)
-tk.Button(root, text="M5", command=banana5).grid(row=1, column=1)
-tk.Button(root, text="M6", command=banana6).grid(row=1, column=2)
-tk.Button(root, text="M7", command=banana7).grid(row=1, column=3)
-tk.Button(root, text="M8", command=banana8).grid(row=1, column=4)
-tk.Button(root, text="M9", command=banana9).grid(row=2, column=1)
-tk.Button(root, text="M10", command=banana10).grid(row=2, column=2)
-tk.Button(root, text="M11", command=banana11).grid(row=2, column=3)
-tk.Button(root, text="M12", command=banana12).grid(row=2, column=4)
-
 root.mainloop()
-
-
+tk.Button(root, text="M1", command=banana1, text "1", width=6, height=4).grid(row=0, column=1)
+tk.Button(root, text="M10", command=banana10,text "10", width=6, height=4).grid(row=2, column=2)
+tk.Button(root, text="M11", command=banana11,text "11", width=6, height=4).grid(row=2, column=3)
+tk.Button(root, text="M12", command=banana12,text "12", width=6, height=4).grid(row=2, column=4)
+tk.Button(root, text="M2", command=banana2,text "2", width=6, height=4).grid(row=0, column=2)
+tk.Button(root, text="M3", command=banana3,text "3", width=6, height=4).grid(row=0, column=3)
+tk.Button(root, text="M4",command=banana4,text "4", width=6, height=4).grid(row=0, column=4)
+tk.Button(root, text="M5", command=banana5,text "5", width=6, height=4).grid(row=1, column=1)
+tk.Button(root, text="M6", command=banana6,text "6", width=6, height=4).grid(row=1, column=2)
+tk.Button(root, text="M7", command=banana7,text "7", width=6, height=4).grid(row=1, column=3)
+tk.Button(root, text="M8", command=banana8,text "8", width=6, height=4).grid(row=1, column=4)
+tk.Button(root, text="M9", command=banana9,text "9", width=6, height=4).grid(row=2, column=1)
